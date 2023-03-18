267238c2f35e849099d0dfe09bd9d460acef3cac
Mary Skywalker <snowdennaruto90@gmail.com>
2023-03-18 16:50:55 -0400

    radix slot, for flexibility with html tags

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,9 +1,22 @@
-interface TextProps {
-    size?: 'sm' | 'md' | 'lg';
-}
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
+import { TextProps } from '../../@types/TextProps';
 
-export function Text({ size = "md" }: TextProps){
+export function Text({ size = "md", children, asChild }: TextProps){
+    const Component = asChild ? Slot : 'span';
+
     return(
-        <span className="text-gray-300 font-sans">Hello world</span>
+        <Component 
+            className={clsx(
+                "text-gray-300 font-sans",
+                {
+                    'text-xs': size === 'sm',
+                    'text-sm': size === 'md',
+                    'text-md': size === 'lg',
+                }
+            )}
+        >
+            {children}
+        </Component>
     );
 }
