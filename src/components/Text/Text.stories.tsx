b4b9034728c359047615397e0209ed3718f68b6c
Mary Skywalker <snowdennaruto90@gmail.com>
2023-03-18 16:49:45 -0400

    text pattern created in storybook

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -1,8 +1,34 @@
+import { Meta, StoryObj } from '@storybook/react';
 import { Text } from ".";
+import { TextProps } from '../../@types/TextProps';
 
 export default {
     title: 'Components/Text',
     component: Text,
+    args: {
+        children: 'hello world'
+    }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    }
 }
 
-export const Default = {}
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: 'lg'
+    }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+    args: {
+        asChild: true,
+        children: (
+            <p>Hello</p>
+        )
+    }
+}
