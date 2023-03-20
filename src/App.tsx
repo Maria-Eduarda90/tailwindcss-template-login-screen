ae8c3315b32eb9fefecb6b1e29ef8b12f6517d69
Mary Skywalker <snowdennaruto90@gmail.com>
2023-03-20 18:50:07 -0400

    finished login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,53 @@
+import { Envelope, Lock } from 'phosphor-react';
+import { Button } from './components/Button';
+import { Heading } from './components/Heading';
+import { Input } from './components/Input';
+import { Text } from './components/Text';
 import './styles/global.css';
+import { Logo } from './svg/Logo';
 
 export function App() {
   return (
-    <h1>Hello world</h1>
+    <div className="w-screen h-screen flex justify-center bg-black text-gray-100">
+      <div className="bg-purple-700 lg:flex-1 flex justify-center items-end">
+        <Logo />
+      </div>
+      <div className="flex-1 flex flex-col justify-center items-center p-12 space-y-6">
+        <header className="flex flex-col items-center">
+          <Heading size='lg'>
+            Seja bem vindo 💜
+          </Heading>
+          <Text size='lg' className='text-gray-500 mt-2'>
+            Faça login em sua conta
+          </Text>
+        </header>
+
+        <form className="flex flex-col gap-4 items-stretch w-full max-w-sm">
+          <label htmlFor="email" className="flex flex-col gap-1">
+            <Text className="font-semibold">E-mail</Text>
+            <Input.Root>
+              <Input.Icon>
+                <Envelope/>
+              </Input.Icon>
+
+              <Input.Input type="email" placeholder="Digite seu e-mail"/>
+            </Input.Root>
+          </label>
+
+          <label htmlFor="password" className="flex flex-col gap-1">
+            <Text className="font-semibold">Senha</Text>
+            <Input.Root>
+              <Input.Icon>
+                <Lock />
+              </Input.Icon>
+
+              <Input.Input type="password" placeholder="Digite sua senha" />
+            </Input.Root>
+          </label>
+
+          <Button type='submit'>Entrar</Button>
+        </form>
+      </div>
+    </div>
   )
-}
+} 
