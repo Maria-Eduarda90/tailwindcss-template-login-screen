90ab3f606b76002ef0c8c88a023d9ab5589566cb
Mary Skywalker <snowdennaruto90@gmail.com>
2023-03-19 20:30:30 -0400

    created component heading

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,22 @@
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
+import { HeadingProps } from '../../@types/HeadingProps';
+
+export function Heading({ size = "md", children, asChild }: HeadingProps){
+    const Component = asChild ? Slot : 'h2';
+
+    return(
+        <Component 
+            className={clsx(
+                "text-gray-300 font-bold",
+                {
+                    'text-lg': size === 'sm',
+                    'text-xl': size === 'md',
+                    'text-2xl': size === 'lg',
+                }
+            )}
+        >
+            {children}
+        </Component>
+    );
+}
